1440298bf71fb14100cb0e47d903f14236dd7a85
Trung Trịnh <trinhhoangtrung194@gmail.com>
2018-04-19 11:14:11 +0700

    change

---
+++ KpiTaskManagement/KpiTaskManagement/TaskManagementForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace KpiTaskManagement
+{
+    partial class TaskManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grcTask = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grcTask)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grcTask
+            // 
+            this.grcTask.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grcTask.Location = new System.Drawing.Point(1, 0);
+            this.grcTask.MainView = this.gridView1;
+            this.grcTask.Name = "grcTask";
+            this.grcTask.Size = new System.Drawing.Size(799, 390);
+            this.grcTask.TabIndex = 0;
+            this.grcTask.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.grcTask;
+            this.gridView1.Name = "gridView1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Location = new System.Drawing.Point(12, 400);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(174, 38);
+            this.btnAddTask.TabIndex = 1;
+            this.btnAddTask.Text = "ADD";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(218, 400);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 38);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "DETAILED/UPDATE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(425, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(174, 38);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "DELETE";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(614, 400);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(174, 38);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "EXIT";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // TaskManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grcTask);
+            this.Name = "TaskManagementForm";
+            this.Text = "Task Management Form";
+            ((System.ComponentModel.ISupportInitialize)(this.grcTask)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl grcTask;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
--- KpiTaskManagement/KpiTaskManagement/TaskManagementForm.cs
+++ KpiTaskManagement/KpiTaskManagement/TaskManagementForm.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace KpiTaskManagement
 {
-    public partial class Form1 : Form
+    public partial class TaskManagementForm : Form
     {
-        public Form1()
+        public TaskManagementForm()
         {
             InitializeComponent();
+        }
+
+        
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
